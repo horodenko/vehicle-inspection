30bed9bf860f85b94eb769908d3897e6d569085b
horodenko <danhorodenko@hotmail.com>
2024-01-23 14:52:20 -0300

    Refactor: making id_veiculo nullable

--- main-project/environment/database/migrations/2024_01_22_232809_add_vehicle_id_to_driver_table.php
+++ main-project/environment/database/migrations/2024_01_22_232809_add_vehicle_id_to_driver_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('motorista', function (Blueprint $table) {
-            $table->foreignId('id_veiculo')->constrained(
+            $table->foreignId('id_veiculo')->nullable()->constrained(
                 table: 'veiculo'
             );
         });
