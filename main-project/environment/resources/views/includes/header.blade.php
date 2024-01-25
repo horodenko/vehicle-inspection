66a4bc900035a7f6c96ef84310aed18a77624dc2
horodenko <danhorodenko@hotmail.com>
2024-01-25 14:12:10 -0300

    Feat: header separate component

--- main-project/environment/resources/views/includes/header.blade.php
+++ main-project/environment/resources/views/includes/header.blade.php
@@ -13,14 +13,6 @@
                 <i class="fa-solid fa-bars bars-icon"></i>
                 </a>
             </li>
-            <li class="user-icon-container">
-                <a href="#" />
-                <i class="fa-solid fa-user user-icon"></i>
-                </a>
-            </li>
-            <li class="arrow-down-container">
-                <a href="#" /><i class="fa-solid fa-caret-down arrow-down-icon"></i></i></span></a>
-            </li>
         </ul>
     </section>
 </body>
